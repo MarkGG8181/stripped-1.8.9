705b82086e1792cd0cf37c043cc37301eade2ac7
noarc <86848614+MarkGG8181@users.noreply.github.com>
2025-07-27 23:59:05 +0300

    Update CrashReport.java

--- src/main/java/net/minecraft/crash/CrashReport.java
+++ src/main/java/net/minecraft/crash/CrashReport.java
@@ -230,9 +230,7 @@ public class CrashReport
     {
         StringBuilder stringbuilder = new StringBuilder();
         stringbuilder.append("---- Minecraft Crash Report ----\n");
-        stringbuilder.append("// ");
-        stringbuilder.append(getWittyComment());
-        stringbuilder.append("\n\n");
+        stringbuilder.append("\n");
         stringbuilder.append("Time: ");
         stringbuilder.append((new SimpleDateFormat()).format(new Date()));
         stringbuilder.append("\n");
@@ -355,23 +353,6 @@ public class CrashReport
 
         this.crashReportSections.add(crashreportcategory);
         return crashreportcategory;
-    }
-
-    /**
-     * Gets a random witty comment for inclusion in this CrashReport
-     */
-    private static String getWittyComment()
-    {
-        String[] astring = new String[] {"Who set us up the TNT?", "Everything\'s going to plan. No, really, that was supposed to happen.", "Uh... Did I do that?", "Oops.", "Why did you do that?", "I feel sad now :(", "My bad.", "I\'m sorry, Dave.", "I let you down. Sorry :(", "On the bright side, I bought you a teddy bear!", "Daisy, daisy...", "Oh - I know what I did wrong!", "Hey, that tickles! Hehehe!", "I blame Dinnerbone.", "You should try our sister game, Minceraft!", "Don\'t be sad. I\'ll do better next time, I promise!", "Don\'t be sad, have a hug! <3", "I just don\'t know what went wrong :(", "Shall we play a game?", "Quite honestly, I wouldn\'t worry myself about that.", "I bet Cylons wouldn\'t have this problem.", "Sorry :(", "Surprise! Haha. Well, this is awkward.", "Would you like a cupcake?", "Hi. I\'m Minecraft, and I\'m a crashaholic.", "Ooh. Shiny.", "This doesn\'t make any sense!", "Why is it breaking :(", "Don\'t do that.", "Ouch. That hurt :(", "You\'re mean.", "This is a token for 1 free hug. Redeem at your nearest Mojangsta: [~~HUG~~]", "There are four lights!", "But it works on my machine."};
-
-        try
-        {
-            return astring[(int)(System.nanoTime() % (long)astring.length)];
-        }
-        catch (Throwable var2)
-        {
-            return "Witty comment unavailable :(";
-        }
     }
 
     /**
