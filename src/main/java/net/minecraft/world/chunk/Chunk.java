d74972f9a6406b5350303ff79e1421bc8bb2cc87
melodeu <165320972+melodeu@users.noreply.github.com>
2025-09-03 15:07:51 +0200

    Optimized getting block state

--- src/main/java/net/minecraft/world/chunk/Chunk.java
+++ src/main/java/net/minecraft/world/chunk/Chunk.java
@@ -462,40 +462,19 @@ public class Chunk implements IChunkLighting, IChunkLightingData, ILightingEngin
         }
     }
 
-    public IBlockState getBlockState(final BlockPos pos) {
-        if (this.worldObj.getWorldType() == WorldType.DEBUG_WORLD) {
-            IBlockState iblockstate = null;
+    public static IBlockState getBlockState(Chunk chunk, BlockPos pos) {
+        final int y = pos.getY();
 
-            if (pos.getY() == 60) {
-                iblockstate = Blocks.barrier.getDefaultState();
-            }
-
-            if (pos.getY() == 70) {
-                iblockstate = ChunkProviderDebug.func_177461_b(pos.getX(), pos.getZ());
-            }
-
-            return iblockstate == null ? Blocks.air.getDefaultState() : iblockstate;
-        } else {
-            try {
-                if (pos.getY() >= 0 && pos.getY() >> 4 < this.storageArrays.length) {
-                    ExtendedBlockStorage extendedblockstorage = this.storageArrays[pos.getY() >> 4];
-
-                    if (extendedblockstorage != null) {
-                        int j = pos.getX() & 15;
-                        int k = pos.getY() & 15;
-                        int i = pos.getZ() & 15;
-                        return extendedblockstorage.get(j, k, i);
-                    }
-                }
-
-                return Blocks.air.getDefaultState();
-            } catch (Throwable throwable) {
-                CrashReport crashreport = CrashReport.makeCrashReport(throwable, "Getting block state");
-                CrashReportCategory crashreportcategory = crashreport.makeCategory("Block being got");
-                crashreportcategory.addCrashSectionCallable("Location", () -> CrashReportCategory.getCoordinateInfo(pos));
-                throw new ReportedException(crashreport);
-            }
+        if (y >= 0 && y >> 4 < chunk.getBlockStorageArray().length) {
+            final ExtendedBlockStorage storage = chunk.getBlockStorageArray()[y >> 4];
+            if (storage != null) return storage.get(pos.getX() & 15, y & 15, pos.getZ() & 15);
         }
+
+        return Blocks.air.getDefaultState();
+    }
+
+    public IBlockState getBlockState(BlockPos pos) {
+        return getBlockState(this, pos);
     }
 
     /**
@@ -562,7 +541,7 @@ public class Chunk implements IChunkLighting, IChunkLightingData, ILightingEngin
 
                     if (j1 > 0) {
                         if (j >= i1) {
-                            this.relightBlock(i, j + 1, k);
+                            this.relightBlock(i, j, k);
                         }
                     } else if (j == i1 - 1) {
                         this.relightBlock(i, j, k);
