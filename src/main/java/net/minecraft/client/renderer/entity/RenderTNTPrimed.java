8c268f200c6f48521a23259529cc58d95bae809a
melodeu <165320972+melodeu@users.noreply.github.com>
2025-09-04 15:53:01 +0200

    Tnt timer

--- src/main/java/net/minecraft/client/renderer/entity/RenderTNTPrimed.java
+++ src/main/java/net/minecraft/client/renderer/entity/RenderTNTPrimed.java
@@ -1,16 +1,25 @@
 package net.minecraft.client.renderer.entity;
 
 import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.util.FontRenderer;
 import net.minecraft.client.renderer.BlockRendererDispatcher;
 import net.minecraft.client.renderer.GlStateManager;
+import net.minecraft.client.renderer.Tessellator;
+import net.minecraft.client.renderer.WorldRenderer;
 import net.minecraft.client.renderer.texture.TextureMap;
+import net.minecraft.client.renderer.vertex.DefaultVertexFormats;
 import net.minecraft.entity.item.EntityTNTPrimed;
 import net.minecraft.init.Blocks;
 import net.minecraft.util.MathHelper;
 import net.minecraft.util.ResourceLocation;
+import org.lwjgl.opengl.GL11;
+
+import java.text.DecimalFormat;
 
 public class RenderTNTPrimed extends Render<EntityTNTPrimed>
 {
+    private static final DecimalFormat decimalFormat = new DecimalFormat("0.00");
+
     public RenderTNTPrimed(RenderManager renderManagerIn)
     {
         super(renderManagerIn);
@@ -61,6 +70,58 @@ public class RenderTNTPrimed extends Render<EntityTNTPrimed>
         }
 
         GlStateManager.popMatrix();
+
+        float remainingTicks = (float)entity.fuse - partialTicks;
+        if (remainingTicks < 0.0F) remainingTicks = 0.0F;
+        float seconds = remainingTicks / 20.0F;
+        String str = decimalFormat.format(seconds);
+        float green = MathHelper.clamp_float(remainingTicks / 80.0F, 0.0F, 1.0F);
+        int r = (int) ((1.0F - green) * 255.0F);
+        int g = (int) (green * 255.0F);
+        int b = 0;
+        int color = 0xFF000000 | (r << 16) | (g << 8) | b;
+        int alphaColor = 0x20000000 | (r << 16) | (g << 8) | b;
+
+        double d0 = entity.getDistanceSqToEntity(this.renderManager.livingPlayer);
+        int maxDistance = 64;
+        if (d0 <= (double)(maxDistance * maxDistance))
+        {
+            FontRenderer fontrenderer = this.getFontRendererFromRenderManager();
+            float f = 1.6F;
+            float f1 = 0.016666668F * f;
+            GlStateManager.pushMatrix();
+            GlStateManager.translate((float)x, (float)y + entity.height + 0.5F, (float)z);
+            GL11.glNormal3f(0.0F, 1.0F, 0.0F);
+            GlStateManager.rotate(-this.renderManager.playerViewY, 0.0F, 1.0F, 0.0F);
+            GlStateManager.rotate(this.renderManager.playerViewX, 1.0F, 0.0F, 0.0F);
+            GlStateManager.scale(-f1, -f1, f1);
+            GlStateManager.disableLighting();
+            GlStateManager.depthMask(false);
+            GlStateManager.disableDepth();
+            GlStateManager.enableBlend();
+            GlStateManager.tryBlendFuncSeparate(770, 771, 1, 0);
+            Tessellator tessellator = Tessellator.getInstance();
+            WorldRenderer worldrenderer = tessellator.getWorldRenderer();
+            int j = 0;
+            GlStateManager.disableTexture2D();
+            worldrenderer.begin(7, DefaultVertexFormats.POSITION_COLOR);
+            int i = fontrenderer.getStringWidth(str) / 2;
+            worldrenderer.pos(-i - 1, -1 + j, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+            worldrenderer.pos(-i - 1, 8 + j, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+            worldrenderer.pos(i + 1, 8 + j, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+            worldrenderer.pos(i + 1, -1 + j, 0.0D).color(0.0F, 0.0F, 0.0F, 0.25F).endVertex();
+            tessellator.draw();
+            GlStateManager.enableTexture2D();
+            fontrenderer.drawString(str, -fontrenderer.getStringWidth(str) / 2, j, alphaColor);
+            GlStateManager.enableDepth();
+            GlStateManager.depthMask(true);
+            fontrenderer.drawString(str, -fontrenderer.getStringWidth(str) / 2, j, color);
+            GlStateManager.enableLighting();
+            GlStateManager.disableBlend();
+            GlStateManager.color(1.0F, 1.0F, 1.0F, 1.0F);
+            GlStateManager.popMatrix();
+        }
+
         super.doRender(entity, x, y, z, entityYaw, partialTicks);
     }
 
